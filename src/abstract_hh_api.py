5919f1e1bc37bb35da6f80b2d8a61cdf9d5a73bb
DaniilKokorev <kokorevdaniil@mail.ru>
2024-02-23 11:46:06 +0700

    абстрактные_классы

--- src/abstract_hh_api.py
+++ src/abstract_hh_api.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 
 class AbstractHHAPI(ABC):
-
+    """ Абстрактный класс для получения API с HeadHunter"""
     @abstractmethod
     def get_vacancies_hh_api(self):
         pass
