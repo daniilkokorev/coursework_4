d96bfdaab14063da79f01e55d364a745f8140476
DaniilKokorev <kokorevdaniil@mail.ru>
2024-02-23 15:04:04 +0700

    ловит_ошибки

--- src/abstract_user_error.py
+++ src/abstract_user_error.py
@@ -2,7 +2,9 @@ from abc import ABC, abstractmethod
 
 
 class UserError(ABC):
-
+    """
+    Класс для проверок ошибок
+    """
     @abstractmethod
     def user_input_int(self):
         pass
---
+++ src/json_user_error.py
@@ -0,0 +1,27 @@
+from abstract_user_error import UserError
+
+
+class JsonUserError(UserError):
+    """
+    Класс отлавливает ошибки при чтении файла json
+    """
+    salary_from = None
+    city = None
+
+    def user_input_int(self):
+        # проверяем правильность ввода зарплаты
+        self.salary_from = input("Введите желаемую зарплату: ")
+        if self.salary_from.isalpha():
+            raise ValueError("Зарплата должна быть числом")
+        elif self.salary_from == "":
+            self.salary_from = 0
+        elif int(self.salary_from) <= 0:
+            raise TypeError("Зарплата должна быть положительным числом")
+        return int(self.salary_from)
+
+    def user_input_str(self):
+        # проверяет правильность ввода названия города
+        self.city = input("Введите название города: ").title()
+        if self.city.isdigit():
+            raise TypeError("Не может быть числом")
+        return self.city
