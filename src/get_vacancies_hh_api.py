ceebe92b225874b7c290a8559c12df2c117312e2
DaniilKokorev <kokorevdaniil@mail.ru>
2024-02-23 11:45:18 +0700

    получаем_вакансии_с_хедхантера

---
+++ src/get_vacancies_hh_api.py
@@ -0,0 +1,24 @@
+import json
+import requests
+from abstract_hh_api import AbstractHHAPI
+from config import DATA
+from vacantions_names import Vacancions
+
+
+class GetvacanciesHHAPI(AbstractHHAPI, Vacancions):
+    """
+    Класс получает список вакансий по API с HH.ru и записывает в json файл
+    """
+    def __init__(self, name_vacancion, page_vacancion):
+        super().__init__(name_vacancion, page_vacancion)
+
+    def get_vacancies_hh_api(self):
+        # получает список вакансий с HH по API
+        key_response = {'text': self.name_vacancion, 'area': 113, 'per_page': self.page_vacancion}
+        response_vacancions = requests.get(f'https://api.hh.ru/vacancies', key_response).json()
+        return response_vacancions
+
+    def write_vacancions_list(self):
+        # записывает вакансии в файл json
+        with open(DATA, 'w', encoding='utf-8') as file:
+            file.write(json.dumps(self.get_vacancies_hh_api(), indent=4, ensure_ascii=False))
