4adb4df85bae243cc14abf4b3ed9c2973feb79e4
DaniilKokorev <kokorevdaniil@mail.ru>
2024-02-27 02:26:21 +0700

    для_работы_с_файлами

---
+++ src/get_vacancion_json.py
@@ -0,0 +1,48 @@
+import json
+import os
+from pprint import pprint
+
+from get_vacancies_hh_api import GetvacanciesHHAPI
+from abstract_json import AbstractJson
+from config import DATA
+
+
+class GetVacancionJson(AbstractJson, GetvacanciesHHAPI):
+    """
+    Класс для работы с файлом
+    """
+
+    def __init__(self, name_vacancion, page_vacancion):
+        super().__init__(name_vacancion, page_vacancion)
+
+    def vacancion_json_write(self):
+        """
+        Записывает вакансии в файл json
+        """
+        with open(DATA, 'w', encoding='utf-8') as file:
+            file.write(json.dumps(self.get_vacancies_hh_api, indent=4, ensure_ascii=False))
+
+    @property
+    def vacancion_json_append(self):
+        """
+        Добавляет вакансии в файл json
+        """
+        with open(DATA, encoding='utf-8') as json_file:
+            vacantion_json_list = json.load(json_file)
+        vacantion_json_list.extend(self.get_vacancies_hh_api)
+        with open(DATA, 'w', encoding='utf-8') as file_app:
+            json.dump(vacantion_json_list, file_app, indent=4, ensure_ascii=False)
+        return vacantion_json_list
+
+    def vacancion_json_clear(self):
+        """
+        Удаляет данные из файла
+        :return:
+        """
+        with open(DATA, 'w', encoding='utf-8') as clear_file:
+            del clear_file
+
+
+if __name__ == '__main__':
+    v = GetVacancionJson('python', 1)
+    pprint(v.vacancion_json_append)
